f36202def433184a0515590738eea0aa50667b8d
laagom <cjyooong@gmail.com>
2023-04-12 00:26:50 +0900

    [Add APPs][Add user, content, gomstagram app] 23-04-12

--- config/views.py
+++ config/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 class Sub(APIView):
     def get(self, request):
         print(f'{__class__} -> get() function!')
-        return render(request, "instagram/main.html")
+        return render(request, "gomstagram/main.html")
 
     def post(self, request):
         print(f' {__class__} -> post() function!')
-        return render(request, "instagram/main.html")
+        return render(request, "gomstagram/main.html")
